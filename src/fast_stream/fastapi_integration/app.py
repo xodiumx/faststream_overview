0cddfdd9e2594adb1ad1a7e7f47321c091b12746
Maks <am.xlexeev@gmail.com>
2024-11-05 08:30:47 +0300

    background handling

--- src/fast_stream/fastapi_integration/app.py
+++ src/fast_stream/fastapi_integration/app.py
@@ -1,18 +1,35 @@
-from fastapi import FastAPI
-from faststream.kafka.fastapi import Logger, KafkaRouter
+import asyncio
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from faststream.kafka.fastapi import Logger, KafkaRouter, KafkaBroker
 
 from settings import settings
 
-router = KafkaRouter(settings.KAFKA_HOST)
 app = FastAPI()
-publisher = router.publisher(settings.KAFKA_TOPIC_PRODUCE)
+router = KafkaRouter(settings.KAFKA_HOST)
 
 
-@publisher
+def broker() -> KafkaBroker:
+    return router.broker
+
+
+async def test_sleep(time: int, logger: Logger) -> None:
+    await asyncio.sleep(time)
+    logger.info("Sleep ended")
+
+
 @router.subscriber(settings.KAFKA_TOPIC_CONSUME)
-async def handler(user_id: int, logger: Logger) -> str:
-    logger.info(user_id)
-    return f"{user_id} created"
+@router.publisher(settings.KAFKA_TOPIC_PRODUCE)
+async def handler(logger: Logger) -> str:
+    await test_sleep(10, logger=logger)
+    return "Message consumed in faststream handler"
+
+
+@router.get("/")
+async def base_route(broker: Annotated[KafkaBroker, Depends(broker)]):
+    await broker.publish("Test consume message", settings.KAFKA_TOPIC_CONSUME)
+    return "Test HTTP"
 
 
 app.include_router(router)
